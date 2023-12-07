04d1b9f570770942d955a7111aa5c8cd821c5ef5
savvy8525 <savannahyost85@gmail.com>
2023-12-07 10:25:20 -0500

    added to strengths page

--- src/pages/DummyPage.jsx
+++ src/pages/DummyPage.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 // import ArrayDisplay from './ArrayDisplay';
 
-const DummyPage = ({growth, setGrowth}) => {
+const DummyPage = ({strengths, setStrengths}) => {
 
     
 
 
     return (
         <div>
-
+{/* 
             <p>impostorSymptom {growth.impostorSymptom}</p>
-            <p>whyThisRole {growth.whyThisRole}</p>
+            <p>whyThisRole {growth.whyThisRole}</p> */}
             {/* <p>skills {growth.skillSet}</p> */}
             
             <ul>
-                {growth.skillSet.map((item) => (
+                {strengths.tools.map((item) => (
+                    <li key={item.index}>
+                        tool: {item.tools}, Level: {item.level}
+                    </li>
+                ))}
+            </ul>
+
+            <ul>
+                {strengths.softSkills.map((item) => (
                     <li key={item.index}>
                         Skills: {item.skills}, Level: {item.level}
                     </li>
